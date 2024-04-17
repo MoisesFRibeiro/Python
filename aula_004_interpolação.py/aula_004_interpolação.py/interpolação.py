bc06b4ce0ad01af547939d65ebc0d9a218f8a252
MoisesFRibeiro <Moises.Senac@outlook.com>
2024-04-17 20:09:42 -0300

    interpolação.py

--- aula_004_interpolação.py/aula_004_interpolação.py/interpolação.py
+++ aula_004_interpolação.py/aula_004_interpolação.py/interpolação.py
@@ -1,6 +1,6 @@
 # imports
 # biblioteca para interargir o SO
-imports os
+import os
 # Biblioteca para pegar data e hora do sistema
 import datetime
 
@@ -13,16 +13,16 @@ print('ENTRADA DE DADOS EM PYTHON')
 print('_'*70)
 
 # Entrada
-nome = input .... Moises
-peso = input .... 80 kg
-altura = input .... 1.65 m
+nome = input ('moises')
+peso = input  ('80 kg')
+altura = input ('1.65 m')
 
 # Entrada com casting
-nascimento = int input .... 1983
-cep = int input .... 36037500
-bairro = str input .... são pedro
-rua = str input joão krolman sobrinho
-numero = int input 8
+nascimento = int  ('1983')
+cep = int   ('36037500')
+bairro = str   ('são pedro')
+rua = str  ('joão krolman sobrinho')
+numero = int  ('8')
 
 # processamento: pegando o ano corrente
 ano_atual = datetime.now().year
@@ -32,13 +32,13 @@ idade = int(ano_atual) - nascimento
 print('_'*70)
 print('SAIDA DE DADOS')
 print('_'*70)
-print('nome..... ' , moises)
+print('nome..... ' , ('moises'))
 print('data de nascimento'....1983)
 print('peso.....' , 80 kg)
 print('altura....' , 1.65 m)
 
 # SAIDA INTERPOLADA
-print(f'{moises}, minha idade{40}anos!')
+print(f'{'moises'}, minha idade{40}anos!')
 print(f'cep...... cep36037500')
 print(f'bairro...... são pedro')
 print(f'rua.........joão krolman sobrinho')
