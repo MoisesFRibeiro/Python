9fd5331dfb3a001eb6bc7ed233d1566847f12194
MoisesFRibeiro <Moises.Senac@outlook.com>
2024-04-26 21:32:44 -0300

    atualizar

---
+++ atividade_002_condicionais/f.py
@@ -0,0 +1,24 @@
+# F) A empresa "LeapYearCheck" está desenvolvendo um software de verificação de anos bissextos para auxiliar usuários na identificação desses anos de forma rápida e precisa.
+# Eles precisam de um programa que permita aos usuários inserir um ano e, em seguida, determine se esse ano é bissexto ou não, de acordo com as regras
+# estabelecidas pelo calendário gregoriano. Além disso, é necessário realizar a validação de entrada de dados para garantir que o ano inserido pelo usuário
+# seja um valor válido, ou seja, um número inteiro positivo.
+
+# curso de desenvolvimento de sistemas 
+# turma 0152
+# autor: moises de souza ribiero
+# data 26/04/2024
+# atividade de condicionais: F
+
+# importando a biblioteca 
+import os
+
+
+# limpando a biblioteca
+os.sytem("cls")
+
+print('_'*70)
+print('atividade: f')
+print('='*70)
+
+# entrada
+ano = input('entre com seu ano ')
