90e62c862fc8345fe19b326b28a007695993070b
moisesfribeiro <moises.senac@outlook.com>
2024-05-09 21:39:37 -0300

    troca

---
+++ atividade_003/a.py
@@ -0,0 +1,18 @@
+# Faça um programa que receba um valor e mostre sua raiz quadrada. Para raízes que não são exatas, arredonde para cima ou para baixo.
+# Faça a validação para números negativos, avisando ao usuário que o resultado será um número complexo.
+
+import os
+import math
+
+os.system('cls')
+
+# entrada
+numero = int(input('entre_com_Numero'))
+
+# processamento
+raizquadrada = math.sqrt('radicando')
+arredondar_para_cima = math.ceil('numero_decimal')
+arredondar_para_baixo = math.floor('numero_decimal')
+
+# saida
+print(f'a raiz quadrada')
