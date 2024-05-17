44775c1d723c8a979ab727ebe07ac61aad433a16
moisesfribeiro <moises.senac@outlook.com>
2024-05-17 19:45:59 -0300

    atual

--- atividade_003/f.py
+++ atividade_003/f.py
@@ -1,9 +1,19 @@
+
+# curso de desenvolvimento de sistemas
+# Turma 0152 ( Braba)
+# Autor: Moises de Souza Ribeiro
+# Data 17/05/2024
+# advinhar numeros
+
 # Faça um programa onde o usuário tenta adivinhar o número que o computador está ‘pensando’.
 
+# importando a biblioteca os
 import os
+# importando a biblioteca random
 import random
 
 
+# limpando o terminal
 os.system('cls')
 
 print('-'*70)
@@ -12,9 +22,11 @@ print('='*70)
 
 print('numero aleatorio inteiro')
 
+# entrada
 numero = int(input('entre com o numero '))
 aleatorio_inteiro = random.randint(1,5)
 
+# saida
 if aleatorio_inteiro == numero:
     print(f'parabens {numero} é igual a {aleatorio_inteiro}')
 
--- atividade_003/g.py
+++ atividade_003/g.py
@@ -1,10 +1,19 @@
+# curso de desenvolvimento de sistemas
+# Turma 0152 ( Braba)
+# Autor: Moises de Souza Ribeiro
+# Data 17/05/2024
+# Raiz quadrada
+
 # Faça um programa que peça os valores de a, b e c de uma equação do 2º grau.
 # Calcule as raízes da equação do 2º grau seguindo a fórmula: Δ = b² - 4ac, x = (-b ± raiz(Δ)) / (2a).
 
+# importando biblioteca
 import os 
+#importando bibliotca math
 import math
 
 
+# limpando o terminal
 os.system('cls')
 
 print('-'*70)
@@ -17,7 +26,6 @@ b =int(input('entre com b '))
 c =int(input('entre com c '))
 
 # processamento
-
 # Δ = b² - 4ac,
 # x = (-b ± raiz(Δ)) / (2a).
 b2 = math.pow(b, 2)
