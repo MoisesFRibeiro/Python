0cedf21ded17d098d3bdecd0f6eada7ae448c6fe
moisesfribeiro <moises.senac@outlook.com>
2024-05-24 21:40:27 -0300

    atual

---
+++ atividade_004_strings/g.py
@@ -0,0 +1,16 @@
+# Faça um programa que receba um número inteiro e mostre na tela:
+# • A quantidade de unidades, a quantidade de dezenas, a quantidade de centenas e a quantidade de milhares.
+
+# curso de desenvolvimento de sistemas 
+# turma 0152(braba)
+# autor: moises de souza ribeiro
+# data: 24/05/2024
+# número inteiro
+# mostrar a quantidade
+
+import os
+
+
+os.system('cls')
+
+numero = int(input('entre com o numero'))
