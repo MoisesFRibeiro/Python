d1f4d1a1432730ab8797bf9e197d721f6d54e303
MoisésFRibeiro <Moiss.Senac@outlook.com>
2024-06-16 22:49:23 -0300

    atual

--- atividade_006_lista/a.py
+++ atividade_006_lista/a.py
@@ -1,1 +1,21 @@
 # Crie a lista [1, 2, 3, 5, 6] e depois insira o valor que está faltando na posição correta.
+import os
+
+
+os.system('cls')
+
+print('-'*70)
+print('criando lista')
+print("-"*70)
+
+lista_numeros = [1, 2, 3, 5, 6]
+lista_numeros.insert()
+for i in range(1):
+    numero = int(input('digite o numero: ')) 
+
+numero_verfificar = int(input('digite o numero: '))
+
+if numero_verfificar in lista_numeros:
+    print(f'o numero {numero_verfificar} esta na lista')
+else:
+    print(f'o numero {numero_verfificar} não esta na lista')    
