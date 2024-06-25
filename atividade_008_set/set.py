1fa8665b514a6e2b970c76a06104bfecc1285623
moisesfribeiro <moises.senac@outlook.com>
2024-06-24 21:41:14 -0300

    atual

---
+++ atividade_008_set/set.py
@@ -0,0 +1,22 @@
+# Trabalho de estrutura de dados SET
+# Senac Minas Gerais/ Juiz de Fora
+# Aluno: Moises De Souza Ribeiro
+# Turma: 0152
+# Ano: 2024
+# Excluindo material escolar de uma lista      
+import os
+
+
+os.system('cls')
+
+# declarando lista de materiais
+materiais_escolar = ["caneta", "lapis", "papel", "borracha"]
+excluir = []
+
+while True:
+
+    excluir = input('selecione qual material deseja excluir( ou digite "s" para sair): ')
+
+    if excluir.lower
+    materiais_escolar.remove('excluir')
+    print(f'a lista de materiais final foi:{materiais_escolar} ')
