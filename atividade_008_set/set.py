667c5faf633ec26abef8ef36570e8498969fffa3
moisesfribeiro <moises.senac@outlook.com>
2024-06-25 21:35:45 -0300

    atualizar

--- atividade_008_set/set.py
+++ atividade_008_set/set.py
@@ -3,11 +3,16 @@
 # Aluno: Moises De Souza Ribeiro
 # Turma: 0152
 # Ano: 2024
-# Excluindo material escolar de uma lista      
+# Excluindo material escolar de uma lista 
+     
 import os
 
 
 os.system('cls')
+
+print('-'*70)
+print('ESTRUTURA DE DADOS: SET')
+print('='*70)
 
 # declarando lista de materiais
 materiais_escolar = ["caneta", "lapis", "papel", "borracha"]
@@ -17,6 +22,11 @@ while True:
 
     excluir = input('selecione qual material deseja excluir( ou digite "s" para sair): ')
 
-    if excluir.lower
-    materiais_escolar.remove('excluir')
+    if excluir.lower():
+     materiais_escolar.remove('')
+
+    elif (excluir != s):
+       print('continue digitando......')
+    
+        
     print(f'a lista de materiais final foi:{materiais_escolar} ')
