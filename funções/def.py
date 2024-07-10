12395a423a198253a852afe87428f3bad94b9d20
moisesfribeiro <moises.senac@outlook.com>
2024-07-09 21:31:43 -0300

    atualizar

---
+++ funções/def.py
@@ -0,0 +1,18 @@
+import os
+
+
+os.system('cls')
+
+# definindo funções
+
+def limpa_tela():
+    """função para limpar o teminal
+    """
+def soma(a, b):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+    """    
+    return a + b
