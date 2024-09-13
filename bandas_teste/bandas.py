fe7bdeabe0a1fd8e51351530804d8bb6ff18c170
MoisésFRibeiro <Moiss.Senac@outlook.com>
2024-09-13 16:12:13 -0300

    bandas

---
+++ bandas_teste/bandas.py
@@ -0,0 +1,35 @@
+import os
+
+
+# super classe
+class Banda:
+    # Método construtor
+    def __init__(self, Legiao):
+        self.legiao = Legiao
+
+    # Método para ser usado na classe filha
+    def Tocar(self):
+        sera = Legiao 
+        pass
+# classe filha
+class Legiao(Banda): 
+    def Tocar(self):
+        Sera = Legiao 
+        return
+
+    print('_'*70):
+    print('Bandas Nacionais')
+    print("_"*70)
+
+    os.system('cls')
+
+    # função que recebe banda e chama o método tocar
+    def Show(Banda):
+        print(Banda.Tocar())
+
+    # criando instancia banda e tocar
+        Sera = Legiao
+
+    # usando polimorfismo
+        Show_Legiao(Tocar)
+print()
